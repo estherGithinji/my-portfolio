ed7f9fbae75a6a15f7837e6c52509008370aab49
esther <esther.githinji@student.moringaschool.com>
2023-04-07 14:28:24 +0300

    deleted files from mixitup

--- index.js
+++ index.js
@@ -29,6 +29,26 @@ const tabs = document.querySelectorAll('[data-target]'),
         })
       })
 
+// ========> MIXITUP FILTER PORTFOLIO <=================
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+// ========> Link Active Work <=================
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork() {
+    linkWork.forEach(l => l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l => l.addEventListener("click", activeWork))
+
 // ========> Work Popup
 
 // ========> SERVICES MODAL
