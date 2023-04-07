1dae10c13aaa60b1d15d57c3189519aed36f2f6e
esther <esther.githinji@student.moringaschool.com>
2023-04-07 13:14:25 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// ========> SHOW SIDEBAR
+
+// ========> SIDEBAR SHOW
+/* Validtes If Constant Exist*/ 
+
+// ========> SIDEBAR HIDDEN
+/* Validtes If Constant Exist*/ 
+
+// ========> SKILLS TABS
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContent = document.querySelectorAll('[data-content]')
+
+      tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            const target = document.querySelector(tab.dataset.target);
+
+            tabContent.forEach(tabContents => {
+                tabContents.classList.remove("skills__active")
+            })
+
+            target.classList.add("skills__active")
+
+
+            tabs.forEach(tab => {
+                tab.classList.remove("skills__active")
+            })
+
+            tab.classList.add("skills__active")
+        })
+      })
+
+// ========> Work Popup
+
+// ========> SERVICES MODAL
+
+// ========> SWIPER TESTIMONIAL
